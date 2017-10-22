2703a339e051fa8affde0c174d40927717e140ec
guyihui <inspnovo@163.com>
2017-10-22 21:27:47 +0800

    build the simple web server

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
+	fmt.Println("hello!")
+	http.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", nil)
 	fmt.Println("hello,vim-go")
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("hello!"))
 }
